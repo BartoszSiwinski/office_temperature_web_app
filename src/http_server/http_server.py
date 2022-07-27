63af396cb5cb4ed91bcd518852c9ef81d2bb973b
Bartosz Siwinski <bartsiwinski@gmail.com>
2022-07-27 21:32:45 +0200

    Swap HttpStatus codes with variable from http module.

--- src/http_server/http_server.py
+++ src/http_server/http_server.py
@@ -1,4 +1,5 @@
 from http.server import BaseHTTPRequestHandler
+from http import HTTPStatus
 
 URL_BASE = 'localhost'
 PORT = 8000
@@ -8,7 +9,7 @@ LOCALHOST_URL = f'{URL_BASE}:{PORT}'
 class Handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        self.send_response(200)
+        self.send_response(HTTPStatus.OK)
         self.send_header('Content-type','text/html')
         self.end_headers()
 
@@ -16,7 +17,7 @@ class Handler(BaseHTTPRequestHandler):
         self.wfile.write(bytes(message, "utf8"))
 
     def do_POST(self):
-        self.send_response(200)
+        self.send_response(HTTPStatus.OK)
         self.send_header('Content-type','text/html')
         self.end_headers()
 
